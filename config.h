4bd4f86c089a043d0394f3f17c11f5e46c70c4c5
szkye <thephantomszkye@gmail.com>
2021-03-06 00:19:56 +0100

    changed tags

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+static const char *tags[] = { "terminal", "web", "whatsapp", "pcman", "五", "六", "七", "八", "九" };
 
 static const Rule rules[] = {
 	/* xprop(1):
