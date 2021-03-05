758b8123766a6cc4a54cf97e3cad33779c0385f3
szkye <thephantomszkye@gmail.com>
2021-03-05 16:22:39 +0100

    fonts & color scheme

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#39375b"; /* bar background color */
 static const char col_gray2[]       = "#39375b"; /* unfocused border */
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#745c97"; /* focused color */
-static const char col_cyan[]        = "#f5b0cb"; /* bar tags */
+static const char col_cyan[]        = "#399952"; /* bar tags */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
