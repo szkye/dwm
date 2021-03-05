e5b44124caa5cf2b369b9281524d2903fc38f81a
szkye <thephantomszkye@gmail.com>
2021-03-05 15:11:31 +0100

    fonts & color scheme

--- config.h
+++ config.h
@@ -6,13 +6,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 6;	/*pixel gap between clients*/
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char *fonts[]          = { "Ubuntu Mono:size=12" };
+static const char dmenufont[]       = "Ubuntu Mono:size=12";
+static const char col_gray1[]       = "#39375b"; /* bar background color */
+static const char col_gray2[]       = "#39375b"; /* unfocused border */
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#745c97"; /* focused color */
+static const char col_cyan[]        = "#745c97"; /* bar tags */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 
 static const Rule rules[] = {
 	/* xprop(1):
