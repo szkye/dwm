876c9b103f1df268fc038abf1b2faa9f7a0a1711
szkye <thephantomszkye@gmail.com>
2021-03-10 17:41:21 +0100

    added systray back

--- config.h
+++ config.h
@@ -19,6 +19,13 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+/* systray */
+
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0:     pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the firs    t monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+
 /* tagging */
 static const char *tags[] = { "terminal", "web", "whatsapp", "pcman", "games", "emu", "code", "read", "misc" };
 
