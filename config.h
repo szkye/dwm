457db382705c69d344f6b34c91dc51fadcbb54ca
szkye <thephantomszkye@gmail.com>
2021-06-24 15:32:46 +0200

    added shorcut to open firefox

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const char dmenufont[]       = "Ubuntu Mono:size=12.5";
 static const char col_gray1[]       = "#44475a"; /* bar background color */
 static const char col_gray2[]       = "#282a36"; /* unfocused border */
 static const char col_gray3[]       = "#f8f8f2";
-static const char col_gray4[]       = "#ff79c6"; /* focused color */
+static const char col_gray4[]       = "#282a36"; /* focused color */
 static const char col_cyan[]        = "#50fa7b"; /* bar tags */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -66,17 +66,21 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+static const char *roficmd[] = { "rofi", "-modi", "drun", "-show", "drun"};
+
+static const char *browsecmd[] = { "firefox", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_i,      spawn,          {.v = browsecmd } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
--- dwm.c
+++ dwm.c
@@ -1902,7 +1902,7 @@ sigchld(int unused)
 void
 spawn(const Arg *arg)
 {
-	if (arg->v == dmenucmd)
+	if (arg->v == roficmd)
 		dmenumon[0] = '0' + selmon->num;
 	if (fork() == 0) {
 		if (dpy)
